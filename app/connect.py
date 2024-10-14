6d61af31ae39e78ed4662f85270b95119b30fc06
Gabriel Venturini <dev.venturini@gmail.com>
2024-10-14 10:56:34 -0300

    add: connection app

---
+++ app/connect.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+def connect(pwd=str):
+    # connects to the database
+    # the param is to make transactions safer asking for the pwd
+    # every time the user tries to connect
+
+    try:
+        database = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password=pwd
+        )
+        return 'Connection established.'
+    except Exception as e:
+        return(e)
+    
+
+if __name__ == "__main__":
+    connect('example123')
