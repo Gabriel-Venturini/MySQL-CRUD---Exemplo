a1c407c630aa4947620078972e017d6262a833fe
Gabriel Venturini <dev.venturini@gmail.com>
2024-10-16 02:55:08 -0300

    update: setup paths

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@
 
 from setuptools import setup, find_packages
 
-setup(name='Basic_CRUD', version='1.2', packages=find_packages())
+setup(name='Basic_CRUD', version='1.3', packages=find_packages())
 
 # run this on terminal with: pip install -e .
